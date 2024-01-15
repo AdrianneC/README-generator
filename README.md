a7b1ca3b2dd1f1da59c85c757341a4676641969c
Adrianne <96875139+AdrianneC@users.noreply.github.com>
2024-01-15 22:04:05 +0000

    included licsense bacged in the function

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
   ## test
   ## Table of Contents
 
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,7 +1,18 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadges = {
+    "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]",
+    "Apache 2.0": "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]",
+    "GPL 3.0": "[![License: GPL 3.0](https://img.shields.io/badge/License-GPLv3-blue.svg)]",
+  };
+
+  const licenseBadge = licenseBadges[data.license] || "";
+
+
   const tableOfContents = `## Table of Contents\n\n- [Installation](#installation)\n- [Usage](#usage)\n- [License](#license)\n- [Contributing](#contributing)\n- [Tests](#tests)\n- [Questions](#questions)\n`;
+  
   return `
+  ${licenseBadge}
   ## ${data.title}
   ${tableOfContents}
 
